645d8c503c8d891020e822d0b5b2746637368ff3
xkzhangsan <xkzhangsan@163.com>
2019-07-20 14:35:11 +0800

    声明式事务基于AOP的注解

--- src/main/java/com/xkzhangsan/tx/service/impl/AccountServiceImpl.java
+++ src/main/java/com/xkzhangsan/tx/service/impl/AccountServiceImpl.java
@@ -1,5 +1,9 @@
 package com.xkzhangsan.tx.service.impl;
 
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.xkzhangsan.tx.dao.AccountDao;
 import com.xkzhangsan.tx.service.AccountService;
 
@@ -11,6 +15,7 @@ public class AccountServiceImpl implements AccountService{
         this.accountDao = accountDao;
     }
     @Override
+    @Transactional(propagation=Propagation.REQUIRED , isolation = Isolation.DEFAULT)
     public void transfer(String outer, String inner, int money) {
         accountDao.out(outer, money);
         int i=10/0;
