54c9225281cc25cff5ab085054c235cf833746c3
xkzhangsan <xkzhangsan@163.com>
2019-07-20 13:19:00 +0800

    init sql

---
+++ src/main/resources/account.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
